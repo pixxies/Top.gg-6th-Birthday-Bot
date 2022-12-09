1fe517fe732e351d468656d98f4c5578b4ed4a3f
pixxies <hello@pixxiesdesign.com>
2022-12-09 20:28:10 +0100

    Test

--- src/buttons/question.ts
+++ src/buttons/question.ts
@@ -1,9 +1,8 @@
 import { ButtonInteraction, Client } from 'discord.js'
 import { query } from '../db'
-import { errorEmbed, infoEmbed, successEmbed } from '../utils/embeds'
-import { isStaffMember } from '../utils/perms'
+import { infoEmbed } from '../utils/embeds'
+// import { isStaffMember } from '../utils/perms'
 import questions from '../assets/quiz/questions.json'
-import { questionTimer } from '../globals'
 
 export const button = {
   name: 'question',
@@ -15,15 +14,15 @@ export const execute = async (
 ) => {
   if (!interaction.inCachedGuild()) return
 
-  if (await isStaffMember(interaction.user))
-    return interaction.reply({
-      embeds: [
-        errorEmbed(
-          `Sorry ${interaction.user.username}! Staff cannot participate!`
-        ),
-      ],
-      ephemeral: true,
-    })
+  // if (await isStaffMember(interaction.user))
+  //   return interaction.reply({
+  //     embeds: [
+  //       errorEmbed(
+  //         `Sorry ${interaction.user.username}! Staff cannot participate!`
+  //       ),
+  //     ],
+  //     ephemeral: true,
+  //   })
 
   const questionIndexStr = interaction.customId.substring(
     interaction.customId.indexOf('_') + 1
@@ -45,59 +44,25 @@ export const execute = async (
     [interaction.user.id, questionIndex + 1]
   )
 
-  if (res.rows.length) {
-    interaction.deferUpdate()
-    return interaction.user.send({
-      embeds: [
-        errorEmbed(
-          `${interaction.user.username}, you've already answered question #${
-            questionIndex + 1
-          }!`
-        ),
-      ],
-    })
-  }
+  if (res.rows.length) return interaction.deferUpdate()
 
   const timeToAnswer = Math.round(
     interaction.createdTimestamp - interaction.message.createdTimestamp
   )
-
-  const timerEnd = questionTimer - timeToAnswer
 
   const correct = question.answer === answerNumber ? 1 : 0
 
   interaction.reply({
     embeds: [
       infoEmbed(
-        `We saved your answer, ${interaction.user.username}!`,
-        `Make sure your DMs are open to see if your answer was correct!`
+        `You answered ${
+          Object.values(question.options)[Number(answerNumber) - 1]
+        }`
       ),
     ],
     ephemeral: true,
     fetchReply: true,
   })
-
-  setTimeout(() => {
-    if (correct === 1)
-      interaction.user.send({
-        embeds: [
-          successEmbed(`You answered question ${questionIndex + 1} correctly!`),
-        ],
-      })
-    else
-      interaction.user.send({
-        embeds: [
-          errorEmbed(
-            `Question #${questionIndex + 1}`,
-            `You answered **${
-              Object.values(question.options)[Number(answerNumber) - 1]
-            }**!\n\nThe correct answer is **${
-              Object.values(question.options)[Number(question.answer) - 1]
-            }**!`
-          ),
-        ],
-      })
-  }, timerEnd)
 
   query(
     `INSERT INTO quiz (userid, question, answer, time, correct) VALUES ($1, $2, $3, $4, $5)`,
